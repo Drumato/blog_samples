04f0f2a6429ff928aa4f9e9bac68aa707df5dcb7
Drumato <drumatech109@gmail.com>
2020-11-18 03:15:14 +0000

    tag

--- dive_into_nom/nom-like-parser/src/Parser.hs
+++ dive_into_nom/nom-like-parser/src/Parser.hs
@@ -1,9 +1,20 @@
-module Parser (character, ParseError(Character)) where
+{-# LANGUAGE LambdaCase #-}
 
-data ParseError = Character
-    deriving (Eq, Show)
+module Parser (character, tag, ParseError (Character, Tag)) where
+
+import Data.List
+
+data ParseError
+  = Character
+  | Tag
+  deriving (Eq, Show)
 
 character :: Char -> String -> Either ParseError (String, Char)
-character pat input = case input of
-    (x: xs) -> if pat == x then Right (xs, x) else Left Character
-    _ -> Left Character
+character pat = \case
+  (x : xs) -> if x == pat then Right (xs, x) else Left Character
+  _ -> Left Character
+
+tag :: String -> String -> Either ParseError (String, String)
+tag pat input = case stripPrefix pat input of
+  Just s -> Right (s, pat)
+  Nothing -> Left Tag
--- dive_into_nom/nom-like-parser/test/Spec.hs
+++ dive_into_nom/nom-like-parser/test/Spec.hs
@@ -1,17 +1,32 @@
+import Parser as P
 import Test.Tasty as Tasty
 import Test.Tasty.HUnit as HUnit
-import Parser as P
 
 main :: IO ()
 main = Tasty.defaultMain tests
 
 tests :: Tasty.TestTree
-tests = Tasty.testGroup "Parser Tests" [ characterTests ]
+tests = Tasty.testGroup "Parser Tests" [characterTests, tagTests]
 
 characterTests :: Tasty.TestTree
-characterTests = Tasty.testGroup "character Tests" [
-    HUnit.testCase "simple character" $
-        HUnit.assertEqual "" (Right ("bc", 'a')) (P.character 'a' "abc")
-    , HUnit.testCase "empty input" $
-    HUnit.assertEqual "" (Left P.Character) (P.character 'a' "")
+characterTests =
+  Tasty.testGroup
+    "character Tests"
+    [ HUnit.testCase "simple character" $
+        HUnit.assertEqual "" (Right ("bc", 'a')) (P.character 'a' "abc"),
+      HUnit.testCase "not matching" $
+        HUnit.assertEqual "" (Left P.Character) (P.character 'a' "bcd"),
+      HUnit.testCase "empty input" $
+        HUnit.assertEqual "" (Left P.Character) (P.character 'a' "")
+    ]
+tagTests :: Tasty.TestTree
+tagTests =
+  Tasty.testGroup
+    "tag Tests"
+    [ HUnit.testCase "simple tag" $
+        HUnit.assertEqual "" (Right ("ato", "Drum")) (P.tag "Drum" "Drumato"),
+      HUnit.testCase "not matching" $
+        HUnit.assertEqual "" (Left P.Tag) (P.tag "Drum" "drumato"),
+      HUnit.testCase "empty input" $
+        HUnit.assertEqual "" (Left P.Tag) (P.tag "a" "")
     ]
