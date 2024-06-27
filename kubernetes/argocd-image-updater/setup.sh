81c1c8e39aef98d544c5df7cd6364cf15330e984
drumato <drumato@pepabo.com>
2024-06-27 17:30:29 +0900

    argocd image updater push
    Signed-off-by: drumato <drumato@pepabo.com>

---
+++ kubernetes/argocd-image-updater/setup.sh
@@ -0,0 +1,30 @@
+set -eux
+
+function create_cluster() {
+  kind create cluster
+  kubectl cluster-info --context kind-kind
+}
+
+function install_argocd() {
+  kubectl create namespace argocd
+  kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+}
+
+function install_argocd() {
+  kubectl create namespace argocd
+  kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+}
+
+function install_argocd_image_updater() {
+  kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/manifests/install.yaml
+}
+
+function install_old_nginx() {
+  kubectl apply -f nginx/old-deployment.yaml
+  kubectl apply -f nginx/old-service.yaml
+}
+
+# create_cluster
+install_argocd
+install_argocd_image_updater
+install_old_nginx
